a812e588de748bf0dc54b68959c568d9140f7180
Omar Razo <girostrike@gmail.com>
2023-06-30 06:51:54 -0700

    backup js made and html linking updated

---
+++ assets/js/script copy.js
@@ -0,0 +1,18 @@
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+// meant as a back-up in case I start messing with the starting code even though we were told not to touch it 
+// (☉ ‿ ⚆) (ㆆ _ ㆆ)
