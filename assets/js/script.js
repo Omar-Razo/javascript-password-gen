1dc7ef64329d647f2e9e4fa8a19fee3523cfaf34
Omar Razo <girostrike@gmail.com>
2023-07-02 19:09:00 -0700

    error handling changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,44 +14,53 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // my code
-
-
-
 function generatePassword() {
-  // choose length btw 8 and 128 characters
-  let passwordLength = prompt("How many characters for your password?");
+  // choose password length (cast to number) and set generated password varible to empty string
+  let passwordLengthInput = Number(prompt("How many characters for your password?"));
   let generatedPassword = "";
   
-  // catches incorrect password length
-  if ((passwordLength < 8) || (passwordLength > 128)) {
-    alert("Invalid password length.\nPassword length must be between 8 and 128 characters.");
-    return "Length Invalid. Please click again to generate a password.";
+  // catches incorrect password input and password length
+  if (Number.isNaN(passwordLengthInput)) {
+    alert("Invalid input.\nplease enter a number")
+    return "Input Invalid. Please click again to generate a password."
+  } else {
+    if ((passwordLengthInput < 8) || (passwordLengthInput > 128)) {
+      alert("Invalid password length.\nPassword length must be between 8 and 128 characters.");
+      return "Length Invalid. Please click again to generate a password.";
+    }
   }
-  // option to include lowercase
+
+  // option to include lowercase characters
   let includeLowercase = confirm("Would you like to include lowercase characters?");
-  // option to include uppercase
+
+  // option to include uppercase characters
   let includeUppercase = confirm("Would you like to include uppercase characters?");
-  // option to include numeric
+
+  // option to include numerical characters
   let includeNumeric = confirm("Would you like to include numerical characters?");
-  // option to include specials
+
+  // option to include specials characters
   let includeSpecial = confirm("Would you like to include special characters?");
-  // catches no options
+
+  // catches no options - at least one option must be chosen
   if ((includeLowercase === false) && (includeUppercase === false) && (includeNumeric === false) && (includeSpecial === false)) {
     alert("Invalid character choices.\nPlease choose at least one type of character.");
-    return "Type Invalid. Please click again to generate a password.";
+    return "Password Type Invalid. Please click again to generate a password.";
   }
-  // arrays of choices
+
+  // strings of each character type
   let lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
   let uppercaseLetters = lowercaseLetters.toUpperCase();
   let numericChoices = "0123456789";
   let specialChoices = "!@#$%^&*()?";
 
+  // arrays from character type strings
   let lowercaseArray = lowercaseLetters.split("");
   let uppercaseArray = uppercaseLetters.split("");
   let numericArray = numericChoices.split("");
   let specialArray = specialChoices.split("");
 
-  // master array logic
+  // master array logic - an array of selected character-type arrays
   let masterArray = []
 
   if (includeLowercase) {
@@ -70,25 +79,14 @@ function generatePassword() {
     masterArray.push(specialArray);
   }
 
-  // master array selection
-
-  // random array from master - returns Array object
+  // random array from master - returns random Array object
   function randomFromMaster () {
     let randomMasterIndex = Math.floor(Math.random() * masterArray.length);
     let randomArrayFromMaster = masterArray[randomMasterIndex];
-    // ---
-    // ---
     return randomArrayFromMaster
   }
 
-  
-  // from array picks random index
-  // function randomIndex(array) {
-  //   let randomArrayIndex = Math.floor(Math.random() * array.length);
-  //   return randomArrayIndex; 
-  // }
-
-  // pick random array index from that array - should return string at random index
+  // picks random array index from that array - should return string at index
   function randomFromArray(array) {
     let randomArrayIndex = Math.floor(Math.random() * array.length);
     let randomArrayChoice = array[randomArrayIndex];
@@ -96,15 +94,9 @@ function generatePassword() {
   }
 
   // generate password
-  while (generatedPassword.length < passwordLength) {
+  while (generatedPassword.length < passwordLengthInput) {
     generatedPassword = generatedPassword + randomFromArray(randomFromMaster());
   }
 
   return generatedPassword;
 }
-
-// START - TESTING
-
-
-
-// END
